231bc8919af9f75ae787f13461167ad863eaf6cc
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-13 09:35:56 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [render, setRender] = useState(0);
+  const [result,setResult] = useState([]);
+  function clickHandle(){
+    setRender(render+1);
+    return (<p id="para">
+    Hello, I've learnt to use the full-stack evaluation tool. This makes me
+    so happy
+  </p>)
+  }
+
+  function clickSubmit(){
+    setResult([...result,clickHandle()]);
+  }
+
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={clickSubmit}>click</button>
+      {result.map((element)=>element)}
     </div>
   );
 }
 
 
 export default App;
+
